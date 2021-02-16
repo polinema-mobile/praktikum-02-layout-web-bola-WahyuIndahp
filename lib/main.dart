61f494ed082edc8976bfc62bd7d13d69461b82c6
= <=>
2021-02-16 21:03:15 +0700

    Praktikum Mengganti Title dan Background Color

--- lib/main.dart
+++ lib/main.dart
@@ -12,58 +12,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Contoh GridView Widget"),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: <Widget>[
-            Container(
-              color: Colors.green,
-              alignment:Alignment.center,
-              child: Text("1", style: TextStyle(fontSize:30,color:
-Colors.white)),
-            ), 
-            Container(
-              color: Colors.red,
-              alignment:Alignment.center,
-              child: Text("2", style: TextStyle(fontSize:30,color:
-Colors.white)),
-              height: 400.0,
-              width: 300.0,
-            ),
-            Container(
-              color: Colors.deepPurple,
-              alignment:Alignment.center,
-              child: Text("3", style: TextStyle(fontSize:30,color:
-Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-            Container(
-              color: Colors.blueGrey,
-              alignment:Alignment.center,
-              child: Text("4", style: TextStyle(fontSize:30,color:
-Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-            Container(
-              color: Colors.cyan[800],
-              alignment:Alignment.center,
-              child: Text("5", style: TextStyle(fontSize:30,color:
-Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-            Container(
-              color: Colors.yellow[800],
-              alignment:Alignment.center,
-              child: Text("6", style: TextStyle(fontSize:30,color:
- Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-          ],
+          title: Text("MyApp"),
+          backgroundColor: Colors.red,
         ),
       ),
     ); 
