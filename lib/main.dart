57f20ee86e4bcba58dc354649effef96a7cb9e04
= <=>
2021-02-16 20:24:34 +0700

    Percobaan Property Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -10,25 +10,24 @@ class MyApp extends StatelessWidget {
     Widget build(BuildContext context) {
       return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Transform")), 
+        appBar: AppBar(title: Text("Contoh Configuration")),
         body: Container(
-            decoration: BoxDecoration(
-              image: const DecorationImage(
-                image: NetworkImage('https://i.imgur.com/a5Ak0vI.jpg'),
-              fit: BoxFit.fitWidth,
-            ),
-            border: Border.all(
-              color: Colors.black,
-              width: 8,
-            ),
+          decoration: BoxDecoration(
+          color: const Color(0xff7c94b6),
+          image: const DecorationImage(
+            image: NetworkImage(
+              'https://pbs.twimg.com/media/EpZHnlfVEAEyMff.jpg'),
+            fit: BoxFit.fitWidth,
           ),
-          height: 200,
-          width: 300,
-          margin: const EdgeInsets.only(left: 30.0, right: 30.0,
-top: 30),
-          transform: Matrix4.rotationZ(-0.1),
-        )
-      ),
+          border: Border.all(
+            color: Colors.black,
+            width: 8,
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        height: 200,
+        width: 300,
+        margin: EdgeInsets.all(20))),
     ); 
   }
 }
