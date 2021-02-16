e0d269c5d0bbf941de77667f9e05c1e4152ebee7
= <=>
2021-02-16 14:12:42 +0700

    Percobaan Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,18 +9,13 @@ class MyApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
       return MaterialApp(
-      home: Container(
-        margin: EdgeInsets.only(top: 30),
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-          AppBar(title: Text('Contoh Cupertino')),
-          CupertinoButton(
-            child: Text("Contoh button"),
-            onPressed: () {},
-          ),
-          CupertinoActivityIndicator(), 
-         ],
+      home: Scaffold(
+        floatingActionButton:FloatingActionButton( 
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        child: Icon(Icons.thumb_up),
+        backgroundColor: Colors.pink,
         ),
       ),
     ); 
